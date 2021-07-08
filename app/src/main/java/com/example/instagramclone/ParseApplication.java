f90c7e0ec3daf679b7b2967baba8e9031fab04a9
Vanessa Agbugba <vanessaagbugba@fb.com>
2021-07-07 19:19:05 -0500

    First Push!

--- app/src/main/java/com/example/instagramclone/ParseApplication.java
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -3,12 +3,16 @@ package com.example.instagramclone;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        //REGISTER PARSE MODEL
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("9WnV4iSpyCeJhDv3ExPN5gfEJM1kPnfjGyrumWVU")
